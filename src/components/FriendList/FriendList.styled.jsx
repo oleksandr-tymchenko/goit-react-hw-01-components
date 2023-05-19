1d39ce9cd8040e4bdf5fe63f7675f1a39f6a1abe
Oleksandr Tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-05-19 08:51:55 +0300

    refuctor function props

--- src/components/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList.styled.jsx
@@ -21,13 +21,11 @@ box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
 `;
 
 
-const setBgColor = props => {
-    if (props.eventType ) {
-        return 'green'
-    } else {
-        return 'red'
-    }
-}
+const setBgColor = props =>
+    
+    props.eventType ? 'green' : 'red';
+
+
 
 export const Item = styled.li`
 display: flex;
